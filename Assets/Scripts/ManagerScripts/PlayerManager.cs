973272c0113014b726f5877b7de927e3ae8eab56
Austin Erhart <austinerhartm@gmail.com>
2025-04-12 02:18:45 -0500

    Player movement update added two more boxcast and checked for edge cases

--- Assets/Scripts/ManagerScripts/PlayerManager.cs
+++ Assets/Scripts/ManagerScripts/PlayerManager.cs
@@ -10,17 +10,22 @@ public class PlayerManager : MonoBehaviour
     [SerializeField] private BoxCollider2D player_collider;
     [SerializeField] private float move_speed = 10f;
     [SerializeField] private float jump_height = 8f;
+    [SerializeField] private float hang_time = 0.12f;
+    [SerializeField] private float jump_buffer_length = 0.07f;
     [SerializeField] private PlayerInput PlayerController;
     [SerializeField] private LayerMask groundLayer;
 
     // Private Variables
+    private Vector2 direction = Vector2.zero;
     private InputAction movement;
     private InputAction jump;
-    private Vector2 direction = Vector2.zero;
-    private bool hasJumped = false;
+    private float hang_counter;
+    private float jump_buffer_counter;
 
     private void Awake()
     {
+
+        player.freezeRotation = true;
         PlayerController = new PlayerInput();
     }
 
@@ -44,10 +49,21 @@ public class PlayerManager : MonoBehaviour
 
         if (IsGrounded())
         {
-            hasJumped = false;
+            hang_counter = hang_time;
+        } else
+        {
+            hang_counter -= Time.deltaTime;
         }
 
-        if(jump.WasReleasedThisFrame() && player.velocity.y > 0)
+        if (jump.WasPressedThisFrame())
+        {
+            jump_buffer_counter = jump_buffer_length;
+        } else
+        {
+            jump_buffer_counter -= Time.deltaTime;
+        }
+
+        if (jump.WasReleasedThisFrame() && player.velocity.y > 0)
         {
             player.velocity = new Vector2(player.velocity.x, player.velocity.y * 0.5f);
         }
@@ -59,15 +75,39 @@ public class PlayerManager : MonoBehaviour
         float dir_x = 0;
         float dir_y = player.velocity.y;
 
-        if (local_direction.y > 0 && IsGrounded() && !hasJumped)
+        if (local_direction.y > 0 && hang_counter > 0f && jump_buffer_counter >= 0)
         {
             dir_y = local_direction.y * jump_height + move_speed;
-            hasJumped = true;
+
+            jump_buffer_counter = 0;
         }
 
         if (local_direction.x != 0)
         {
             dir_x = local_direction.x * move_speed;
+        }
+
+        if (IsWallLeft())
+        {
+            if(local_direction.x < 0)
+            {
+                dir_x = 0;
+            } else
+            {
+                dir_x = local_direction.x * move_speed;
+            }
+        }
+
+        if (IsWallRight())
+        {
+            if (local_direction.x > 0)
+            {
+                dir_x = 0;
+            }
+            else
+            {
+                dir_x = local_direction.x * move_speed;
+            }
         }
 
         player.velocity = new Vector2(dir_x, dir_y);
@@ -76,10 +116,34 @@ public class PlayerManager : MonoBehaviour
     private bool IsGrounded()
     {
         float extraHeightTest = 0.1f;
+
         RaycastHit2D raycast_hit = Physics2D.BoxCast(player_collider.bounds.center, player_collider.bounds.size, 0f, Vector2.down, extraHeightTest, groundLayer);
 
         return raycast_hit.collider != null;
-
     }
+
+    private bool IsWallLeft()
+    {
+        float extraLengthTest = 0.1f;
+
+        Vector2 box_size = player_collider.bounds.size;
+        box_size.y = box_size.y * 0.9f;
+        RaycastHit2D raycast_left_hit = Physics2D.BoxCast(player_collider.bounds.center, box_size, 0f, Vector2.left, extraLengthTest, groundLayer);
+
+        return raycast_left_hit.collider != null;
+    }
+
+    private bool IsWallRight()
+    {
+        float extraLengthTest = 0.1f;
+
+        Vector2 box_size = player_collider.bounds.size;
+        box_size.y = box_size.y * 0.9f;
+        RaycastHit2D raycast_left_hit = Physics2D.BoxCast(player_collider.bounds.center, box_size, 0f, Vector2.right, extraLengthTest, groundLayer);
+
+        return raycast_left_hit.collider != null;
+    }
+
+
 
 }
