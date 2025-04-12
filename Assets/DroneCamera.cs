0518f1c6d5e0badff9432983c7b0df8e388d67df
Quentin Quarles <quentavius115@gmail.com>
2025-04-12 01:11:46 -0500

    Drone Camera
    Drone camera example and script

---
+++ Assets/DroneCamera.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class DroneCamera : MonoBehaviour
+{
+    public GameObject drone;
+    public Vector2 increment = new Vector2(1f, 0f);
+    public int leftbound;
+    public int rightbound;
+    public bool left;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(!left){
+            transform.Translate(-increment * Time.deltaTime);
+            if(drone.transform.position.x < leftbound){
+                transform.Rotate(0, 180, 0);
+                left = true;
+            }
+        }
+        if(left){
+            transform.Translate(-increment * Time.deltaTime);
+            if(drone.transform.position.x > rightbound){
+                transform.Rotate(0,-180,0);
+                left = false;
+            }
+        }
+
+    }
+}
+
+    //public IEnumerator Movement(){
+        
+    //}
+
