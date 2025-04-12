60214b6da736be8e3379ba1f5ae7c67cefeb7535
ealberty <101431003+ealberty@users.noreply.github.com>
2025-04-12 14:33:27 -0500

    stealth set 4 is done

--- Assets/Scripts/Flicker.cs
+++ Assets/Scripts/Flicker.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 using System.Threading;
 using UnityEngine;
 
@@ -8,10 +9,11 @@ public class Flicker : MonoBehaviour
     public GameObject vision;
     public int delay;
     public int flicker;
+    public int numFlickers; 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(OnAndOff());
+        StartCoroutine(flickering());
     }
 
     // Update is called once per frame
@@ -20,18 +22,26 @@ public class Flicker : MonoBehaviour
         
     }
 
-    public IEnumerator OnAndOff(){
+    public IEnumerator flickering(){
         while(true){
-            yield return new WaitForSeconds(delay);
-            vision.SetActive(false);
-
-            yield return new WaitForSeconds(.1f);
-            vision.SetActive(true);
-            yield return new WaitForSeconds(.1f);
-            vision.SetActive(false);
-            yield return new WaitForSeconds(delay);
-            vision.SetActive(true);
+            yield return StartCoroutine(OnAndOff(0.3f));
+            yield return StartCoroutine(OnAndOff(0.7f));
+            yield return StartCoroutine(OnAndOff(1.4f));
         }
+    }
+
+    public IEnumerator OnAndOff(float stepDelay){
+        yield return new WaitForSeconds(delay * stepDelay);
+        vision.SetActive(false);
+        for (int i = 0; i < numFlickers; i++){
+            yield return new WaitForSeconds(.1f);
+            vision.SetActive(true);
+            yield return new WaitForSeconds(.1f);
+            vision.SetActive(false);
+        }
+        
+        yield return new WaitForSeconds(delay);
+        vision.SetActive(true);
         
     }
 }
