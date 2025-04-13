b9654d585afed6d5bae0b96abb89ee2b47f45b69
ealberty <101431003+ealberty@users.noreply.github.com>
2025-04-13 01:06:20 -0500

    radio tower

---
+++ Assets/Scripts/movePlatformUpAndDown.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class VerticalPlatformMover : MonoBehaviour
+{
+    public float moveDistance = 2f;       // How far to move up and down
+    public float moveSpeed = 1f;          // Speed of movement
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // PingPong returns a value that goes back and forth between 0 and moveDistance
+        float newY = startPosition.y + Mathf.PingPong(Time.time * moveSpeed, moveDistance);
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
+}
+
